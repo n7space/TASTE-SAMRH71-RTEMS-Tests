76dd282c33d1d16181e19884767840ec1c6cd330
Rafał Babski <rbabski@n7space.com>
2026-04-24 10:34:54 +0200

    test_samrh71_rtems_cpu_config_off added verification that PMC is not configured

--- tests/test_samrh71_rtems_cpu_config_off.py
+++ tests/test_samrh71_rtems_cpu_config_off.py
@@ -15,7 +15,44 @@ def test_samrh71_rtems_cpu_config_off():
     stderr = build.stderr.decode("utf-8")
     assert build.returncode == 0, f"Compilation errors: \n{stderr}"
 
-    common.run_verification_project(remote_gdb_server, 'samrh71-rtems-cpu-config-off/samrh71-rtems-rt-rtos-no-init/work/binaries/partition_1', 'pinger.c', '17')
+    gdbmi = GdbController(command=["gdb-multiarch", "--interpreter=mi2"])
+
+    gdbmi.write(f"target extended-remote {remote_gdb_server}")
+    project_bin = 'samrh71-rtems-cpu-config-off/samrh71-rtems-rt-rtos-no-init/work/binaries/partition_1'
+    gdbmi.write(f"file {project_bin}")
+    common.target_extended_reset(gdbmi)
+    gdbmi.write("load")
+    gdbmi.write("break pinger_trap")
+    # break all pmc functions which can be used to configure clocks
+    gdbmi.write("break Pmc_setConfig")
+    gdbmi.write("break Pmc_setMainckConfig")
+    gdbmi.write("break Pmc_setPllConfig")
+    gdbmi.write("break Pmc_setMasterckConfig")
+    gdbmi.write("break Pmc_getRc2OscillatorConfig")
+    gdbmi.write("continue")
+
+    stopped = False
+    iteration = 0
+    while not stopped and iteration < 10:
+        iteration = iteration + 1
+        responses = gdbmi.get_gdb_response(timeout_sec=5)
+        for msg in responses:
+            if msg["type"] == "notify" and msg["message"] == "stopped":
+                payload = msg["payload"]
+                if payload["reason"] == "breakpoint-hit":
+                    assert payload["frame"]["func"] == "pinger_trap"
+                    stopped = True
+
+    test_result = gdbmi.write("-data-evaluate-expression test_result")
+    value = None
+    for msg in test_result:
+        if msg["type"] == "result" and msg["message"] == "done":
+            payload = msg.get("payload", {})
+            if "value" in payload:
+                value = payload["value"]
+
+    assert value == "true", f"Test execution errors: \n test_result = {value}"
+
 
 if __name__ == "__main__":
     test_samrh71_rtems_cpu_config_off()
