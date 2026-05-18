c4570dd41280674252cde6864ffde6551ddfaf69
Rafał Babski <rbabski@n7space.com>
2026-05-18 11:39:04 +0200

    Added test samrh71-rtems-n-m-communication

---
+++ tests/test_samrh71_rtems_n_m_communication.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import common
+import time
+import os
+from pygdbmi.gdbcontroller import GdbController
+
+
+def test_samrh71_rtems_n_m_communication():
+    common.do_clean_build("samrh71-rtems-n-m-communication")
+    remote_gdb_server = os.getenv("SAMRH71_REMOTE_GDBSERVER", default="127.0.0.1")
+
+    build = common.do_build("samrh71-rtems-n-m-communication", ["samrh71", "debug"])
+    stderr = build.stderr.decode("utf-8")
+    assert build.returncode == 0, f"Compilation errors: \n{stderr}"
+
+    common.run_verification_project(remote_gdb_server, 'samrh71-rtems-n-m-communication/work/binaries/partition_1', 'testfunction.c', '29')
+
+if __name__ == "__main__":
+    test_samrh71_rtems_n_m_communication()
