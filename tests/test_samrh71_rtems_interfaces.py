809a1d625dbadc850cd6b4d84363eef73f44fb1b
Rafał Babski <rbabski@n7space.com>
2026-05-18 10:13:24 +0200

    Added samrh71-rtems-interfaces

---
+++ tests/test_samrh71_rtems_interfaces.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import common
+import time
+import os
+from pygdbmi.gdbcontroller import GdbController
+
+
+def test_samrh71_rtems_interfaces():
+    common.do_clean_build("samrh71-rtems-interfaces")
+    remote_gdb_server = os.getenv("SAMRH71_REMOTE_GDBSERVER", default="127.0.0.1")
+
+    build = common.do_build("samrh71-rtems-interfaces", ["samrh71", "debug"])
+    stderr = build.stderr.decode("utf-8")
+    assert build.returncode == 0, f"Compilation errors: \n{stderr}"
+
+    common.run_verification_project(remote_gdb_server, 'samrh71-rtems-interfaces/work/binaries/partition_1', 'testfunction.c', '22')
+
+if __name__ == "__main__":
+    test_samrh71_rtems_interfaces()
