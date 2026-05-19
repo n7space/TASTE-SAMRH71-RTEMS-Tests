7b4e184a4266d3a9638eb0c0133f5e4dce0a1e5b
Rafał Babski <rbabski@n7space.com>
2026-05-19 08:54:42 +0200

    Added project samrh71-rtems-cyclic-priority

---
+++ tests/test_samrh71_rtems_cyclic_priority.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import common
+import time
+import os
+from pygdbmi.gdbcontroller import GdbController
+
+
+def test_samrh71_rtems_cyclic_priority():
+    common.do_clean_build("samrh71-rtems-cyclic-priority")
+    remote_gdb_server = os.getenv("SAMRH71_REMOTE_GDBSERVER", default="127.0.0.1")
+
+    build = common.do_build("samrh71-rtems-cyclic-priority", ["samrh71", "debug"])
+    stderr = build.stderr.decode("utf-8")
+    assert build.returncode == 0, f"Compilation errors: \n{stderr}"
+
+    common.run_verification_project(remote_gdb_server, 'samrh71-rtems-cyclic-priority/work/binaries/partition_1', 'testfunction.c', '48')
+
+if __name__ == "__main__":
+    test_samrh71_rtems_cyclic_priority()
