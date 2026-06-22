3036043c22c6c61dd8a7fa266594aa7219dcefd0
Rafał Babski <rbabski@n7space.com>
2026-06-22 13:09:19 +0200

    Added test test_samrh71_rtems_twin_serial.py && fixes in test_samrh71_rtems_serial.py

--- tests/test_samrh71_rtems_twin_serial.py
+++ tests/test_samrh71_rtems_twin_serial.py
@@ -12,17 +12,17 @@ from pygdbmi.gdbcontroller import GdbController
     not os.getenv("SAMRH71_RTEMS_SERIAL_ENABLED"),
     reason="Serial is not enabled on current platform",
 )
-def test_samrh71_rtems_serial():
-    common.do_clean_build("samrh71-rtems-serial")
+def test_samrh71_rtems_twin_serial():
+    common.do_clean_build("samrh71-rtems-twin-serial")
     remote_gdb_server = os.getenv("SAMRH71_REMOTE_GDBSERVER", default="127.0.0.1")
-    build = common.do_build("samrh71-rtems-serial", ["samrh71", "debug"])
+    build = common.do_build("samrh71-rtems-twin-serial", ["samrh71", "debug"])
     stderr = build.stderr.decode("utf-8")
     assert build.returncode == 0, f"Compilation errors: \n{stderr}"
 
     gdbmi = GdbController(command=["gdb-multiarch", "--interpreter=mi2"])
     try:
         gdbmi.write(f"target extended-remote {remote_gdb_server}")
-        gdbmi.write("file samrh71-rtems-serial/work/binaries/partition_1")
+        gdbmi.write("file samrh71-rtems-twin-serial/work/binaries/partition_2")
         common.target_extended_reset(gdbmi)
         gdbmi.write("load")
         gdbmi.write("continue")
@@ -31,16 +31,14 @@ def test_samrh71_rtems_serial():
         time.sleep(2)
 
         expected = [
-            "Sent ping  0",
-            "Got pong  0",
-            "Sent ping  1",
-            "Got pong  1",
-            "Sent ping  2",
-            "Got pong  2",
+            "Got response from UART3: 1",
+            "Got response from UART3: 2",
+            "Got response from UART1: 3",
+            "Got response from UART1: 4",
         ]
 
         errors = common.do_execute(
-            "samrh71-rtems-serial", expected, test_exe="work/binaries/partition_2"
+            "samrh71-rtems-twin-serial", expected, test_exe="work/binaries/partition_1"
         )
     finally:
         gdbmi.exit()
@@ -48,4 +46,4 @@ def test_samrh71_rtems_serial():
 
 
 if __name__ == "__main__":
-    test_samrh71_rtems_serial()
+    test_samrh71_rtems_twin_serial()
