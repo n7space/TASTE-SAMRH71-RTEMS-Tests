013516ab2e48b290c832ac6d11076f4cf32b9e0a
Rafał Babski <rbabski@n7space.com>
2026-06-24 10:42:44 +0200

    MBEP-259 samrh71-rtems-fpu

---
+++ tests/test_samrh71_rtems_fpu.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import common
+import time
+import os
+from pygdbmi.gdbcontroller import GdbController
+
+
+def test_samrh71_rtems_fpu():
+    common.do_clean_build("samrh71-rtems-fpu")
+    remote_gdb_server = os.getenv("SAMRH71_REMOTE_GDBSERVER", default="127.0.0.1")
+
+    build = common.do_build("samrh71-rtems-fpu", ["samrh71", "debug"])
+    stderr = build.stderr.decode("utf-8")
+    assert build.returncode == 0, f"Compilation errors: \n{stderr}"
+
+    common.run_verification_project(remote_gdb_server, 'samrh71-rtems-fpu/work/binaries/partition_1', 'testfunction.c', '37')
+
+if __name__ == "__main__":
+    test_samrh71_rtems_fpu()
