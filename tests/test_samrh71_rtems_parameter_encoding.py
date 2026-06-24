a55ff6d609fbe423c84a94671919d18923bbf44e
Rafał Babski <rbabski@n7space.com>
2026-06-24 10:47:47 +0200

    MBEP-267 samrh71-rtems-parameter-encoding

---
+++ tests/test_samrh71_rtems_parameter_encoding.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import common
+import time
+import os
+from pygdbmi.gdbcontroller import GdbController
+
+
+def test_samrh71_rtems_parameter_encoding():
+    common.do_clean_build("samrh71-rtems-parameter-encoding")
+    remote_gdb_server = os.getenv("SAMRH71_REMOTE_GDBSERVER", default="127.0.0.1")
+
+    build = common.do_build("samrh71-rtems-parameter-encoding", ["samrh71", "debug"])
+    stderr = build.stderr.decode("utf-8")
+    assert build.returncode == 0, f"Compilation errors: \n{stderr}"
+
+    common.run_verification_project(remote_gdb_server, 'samrh71-rtems-parameter-encoding/work/binaries/partition_1', 'testfunction.c', '81')
+
+if __name__ == "__main__":
+    test_samrh71_rtems_parameter_encoding()
